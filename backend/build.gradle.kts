69ca853cd8a3af37a9e296438ff3395017031d1c
sylhare <sylhare@outlook.com>
2022-11-16 09:12:48 -0500

    Update Kotlin version

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-  val kotlinVersion = "1.5.10"
+  val kotlinVersion = "1.6.20"
   kotlin("jvm") version kotlinVersion
   kotlin("plugin.spring") version kotlinVersion
   id("org.springframework.boot") version "2.3.12.RELEASE"
